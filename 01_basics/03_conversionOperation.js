1a05c81d6033fd587053c39edb5be09e7abd87d2
souvik <souvik@12>
2025-09-01 17:26:17 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,6 +23,11 @@ let booleanIsLoogedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+//  *************************** operations ************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
