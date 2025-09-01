b883fcaa9d71ea9c81442e08130678b179fecacb
souvik <souvik@12>
2025-09-01 17:10:55 +0530

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc" // so alwways check the route of the value(number,string)
+
+// console.log(typeof score)
+// console.log(typeof (score)) // same as above but in method form we can use both above one or this 
+
+let valueInNumber = Number(score) // converting string to number by declaring manually
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33aba" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoogedIn)
+
+// 1 => true, 0 => false
+// "" => false
+// "souvik" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
