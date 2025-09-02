a503b5ff590777f22d9d7070a48bed4e257e91e9
souvik <souvik@12>
2025-09-02 18:27:14 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,39 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-console.log(negValue)
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " souvik"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log( "1" + 2) // catching string first
+// console.log(1 + "2") // catching number first
+// console.log("1" + 2 + 2) // cathing string firsrt and declaring all asss strings 
+// console.log(1 + 2 + "2") // catching number first and declaring all as strings
+
+
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
+
+
+
+
+ 
 
