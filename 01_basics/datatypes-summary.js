e0ab3f666a1e02fbe5450884267a1bdbba3ecf55
souvik <souvik@12>
2025-09-06 21:38:31 +0530

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive data types:
+
+// 7 types: string, number, boolean, null, undefined, symbol, bigint
+
+const marks = 80
+const marksrev = 82.5
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail 
+
+const id =Symbol('158')
+const anotherId = Symbol('158')
+
+console.log(id === anotherId)
+
+const bigNumber = 3737327473070470237n  // bigint example 
+
+
+
+// reference (non primitive) data types:
+
+
+//  object, array, function
+
+const heros = ["thor", "ironman", "hulk"]  // array example
+
+let myObj = {
+    name : "souvik",
+    age : 21,
+
+} // object example
+
+const myFunction = function(){
+    console.log("hello souvik")
+    
+} // function example
+
+
+
+
+
