899907448d24741c88ea816e2cc482bbd6b62ba5
souvik <souvik@12>
2025-09-21 17:24:45 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -59,7 +59,48 @@ function loginUserMessage(username){
 
 }
 
-console.log(loginUserMessage()) // it will give you undefined username cuz we haven't declared any name 
+// console.log(loginUserMessage()) // it will give you undefined username cuz we haven't declared any name to print 
+// console.log(loginUserMessage("souvik"))
+ 
 
+// function example of rest operator
+
+function calculateCartPrice(val1, val2, ...num1){ // ... is used as a rest operator here not as a spread operator, and val1 and val2 are not included int the output cuz ... rest operator is used after val1,val2 
+    return num1
+
+}
+
+// console.log(calculateCartPrice(100,250,550,2000,4000))
+
+// passing object in function
+
+const user = {
+    username: "souvik",
+    price: 399
+
+}
+
+function handleObject(anyobject){
+    // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+    
+
+}
+
+// handleObject(user)
+handleObject({
+    username: "souvik",
+    price: 499
+}) // another way of executing for giving value by creating object within the function
+
+
+// passing arrays in functions
+
+const myNewArray = [200, 480, 700, 988]
+
+function returnSecondValue(getArray){ 
+    return getArray[1] // it will only print the 2nd value from the array with 0th index so it will print 480 as 2nd value 
+
+}
+console.log(returnSecondValue(myNewArray))
 
 
